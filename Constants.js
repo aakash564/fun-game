179cf60bc770d3133a8c6f4c505c7854af2ce8a3
Aakash Chaudhary <81179090+aakash564@users.noreply.github.com>
2025-11-08 20:50:21 +0545

    Create Constants.js

---
+++ Constants.js
@@ -0,0 +1,10 @@
+export const GAME_WIDTH = 800;
+export const GAME_HEIGHT = 600;
+export const PLAYER_SIZE = 15;
+export const PLAYER_SPEED = 3;
+export const COMET_SIZE = 8;
+export const MAX_COMETS = 50;
+export const INITIAL_SCORE = 0;
+export const SCORE_PER_COMET = 1;
+export const UPDATE_INTERVAL_MS = 1000 / 60; // 60 FPS target
+
